ffc7c434b2ff497dd252d278533ab9e994279c17
Raditya Dwisatrio Santoso <dwisatrioraditya@gmail.com>
2025-10-01 09:42:58 +0700

    feat: creating main container and a boilerplate form

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import { Poppins } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import Container from "@/components/container";
 
 const poppins = Poppins({
   variable: "--font-poppins",
@@ -9,10 +10,23 @@ const poppins = Poppins({
 
 export default function Home() {
   return (
-    <>
-      <main className={`${styles.main} ${poppins.variable}`}>
-
-      </main>
-    </>
+    <main style={{
+      height: "100vh",
+    }} className={`${styles.main} ${poppins.variable}`}>
+      <Container>
+        <div>
+          <h1>Sign Up Flow</h1>
+          <form>
+            <input type="email" placeholder="Email" required />
+            <br />
+            <input type="password" placeholder="Password" required />
+            <br />
+            <input type="password" placeholder="Confirm Password" required />
+            <br />
+            <button type="submit">Sign Up</button>
+          </form>
+        </div>
+      </Container>
+    </main>
   );
 }
